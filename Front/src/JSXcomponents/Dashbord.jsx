23baea1f652452828d3776b324cb5194021e0e0c
Nejmeelldine <dn.hadrouga@esi-sba.dz>
2025-03-08 17:48:38 +0100

    Update Dashbord.jsx

--- Front/src/JSXcomponents/Dashbord.jsx
+++ Front/src/JSXcomponents/Dashbord.jsx
@@ -7,7 +7,7 @@ const Dashboard = () => {
     <div className={styles.contenta}>
       <div className={styles.logo}>logo</div> 
       <h2 className={styles.wh2}>Welcome to the Admin Panel</h2>
-      <p className={styles.paragr}>Manage everything with ease.</p> 
+      <h2 className={styles.paragr}>Manage everything with ease.</h2> 
       <button className={styles.getstarted}>Get Started</button> 
     </div>
   </div>
